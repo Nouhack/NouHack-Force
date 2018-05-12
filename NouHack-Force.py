b429b06b275a5d918317a45fd227d061719f91e9
Nouhack <35781319+Nouhack@users.noreply.github.com>
2018-05-13 00:09:40 +0200

    Add files via upload

---
+++ NouHack-Force.py
@@ -0,0 +1,235 @@
+from termcolor import *
+print("                   <==========================================>")
+print('                   __        __   _                          ')
+print('                   \ \      / /__| | ___ ___  _ __ ___   ___ ')
+print('                    \ \ /\ / / _ \ |/ __/ _ \| ''_ ` _ \ / _ `\ ')
+print('                     \ V  V /  __/ | (_| (_) | | | | | |  __/')
+print('                      \_/\_/ \___|_|\___\___/|_| |_| |_|\___|')
+print('                                                                ')
+print('                   <===========================================>')
+print(      colored('              NouHack-Force Was Created By Nouhack Team at 12/05/2018 \n      before using This Script You Need to search Most Important Information \n            About Your Vectime {Social Engineering} to have good results\n NouHack-Force Scripte Use A Smart Algorithme To Generate A Small Powerfull List Of PassWOrd','red'))
+class MyApp:
+    def __init__(self,name,prenom,jour,mois,anné):
+        self.name=name
+        self.prenom=prenom
+        self.jour=jour
+        self.mois=mois
+        self.anné=anné
+        self.date=self.jour+self.mois+self.anné
+
+    def function(self):
+######################################################
+        for i in range(len(self.name)+1):
+            print(self.prenom+self.name[0:i])
+            ########################
+        for i in range(len(self.name)+1):
+            print(self.name[0:i]+self.prenom)
+
+###########################################
+        for i in range(len(self.jour)+1):
+            print(self.prenom+self.jour[0:i])
+            ##################
+        for i in range(len(self.jour)+1):
+            print(self.jour[0:i]+self.prenom)
+            ##################
+        for i in range(len(self.prenom)+1):
+            print(self.jour+self.prenom[0:i])
+            #################
+        for i in range(len(self.prenom)+1):
+            print(self.prenom[0:i]+self.jour)
+
+        for i in range(len(self.mois)+1):
+            print(self.prenom+self.mois[0:i])
+            #################
+        for i in range(len(self.mois)+1):
+            print(self.mois[0:i]+self.prenom)
+            #####################
+        for i in range(len(self.prenom)+1):
+            print(self.mois+self.prenom[0:i])
+            ##################
+        for i in range(len(self.prenom)+1):
+            print(self.prenom[0:i]+self.mois)
+########################################################
+        for i in range(len(self.anné)+1):
+            print(self.prenom+self.anné[0:i])
+            #################
+        for i in range(len(self.anné)+1):
+            print(self.anné[0:i]+self.prenom)
+            ####################
+        for i in range(len(self.prenom)+1):
+            print(self.anné+self.prenom[0:i])
+            ##################*
+        for i in range(len(self.prenom)+1):
+            print(self.prenom[0:i]+self.anné)
+########################################################
+        for i in range(len(self.prenom)+1):
+            print(self.name+self.prenom[0:i])
+            ###############
+        for i in range(len(self.prenom)+1):
+            print(self.prenom[0:i]+self.name)
+
+        for i in range(len(self.jour)+1):
+            print(self.name+self.jour[0:i])
+            ################
+        for i in range(len(self.jour)+1):
+            print(self.jour[0:i]+self.name)
+            #################
+        for i in range(len(self.name)+1):
+            print(self.jour+self.name[0:i])
+            #################
+        for i in range(len(self.name)+1):
+            print(self.name[0:i]+self.jour)                     #hadi ta3 name
+                                                                #
+        for i in range(len(self.mois)+1):
+            print(self.name+self.mois[0:i])
+            #################
+        for i in range(len(self.mois)+1):
+            print(self.mois[0:i]+self.name)
+            ################
+        for i in range(len(self.name)+1):
+            print(self.mois+self.name[0:i])
+            ##############
+        for i in range(len(self.name)+1):
+            print(self.name[0:i]+self.mois)
+
+        for i in range(len(self.anné)+1):
+            print(self.name+self.anné[0:i])
+            #################
+        for i in range(len(self.anné)+1):
+            print(self.anné[0:i]+self.name)
+            ################
+        for i in range(len(self.name)+1):
+            print(self.anné+self.name[0:i])
+            ##################
+        for i in range(len(self.name)+1):
+            print(self.name[0:i]+self.anné)
+########################################################
+        for i in range(len(self.date)+1):
+            print(self.name+self.date[0:i])
+            ###########################
+        for i in range(len(self.date)+1):
+            print(self.date[0:i]+self.name)    ############### hadi name + date
+
+
+        for i in range(len(self.name)+1):
+            print(self.date+self.name[0:i])
+            ###################
+        for i in range(len(self.name)+1):
+            print(self.name[0:i]+self.date)
+#######################################################
+        for i in range(len(self.date)+1):
+            print(self.prenom+self.date[0:i])
+            ###########################
+        for i in range(len(self.date)+1):
+            print(self.date[0:i]+self.prenom)    ############### hadi name + date
+
+
+        for i in range(len(self.prenom)+1):
+            print(self.date+self.prenom[0:i])
+            ###################
+        for i in range(len(self.prenom)+1):
+            print(self.prenom[0:i]+self.date)
+######################################################
+        for i in range(len(self.date)+1):
+            print(self.name+self.prenom+self.date[0:i])
+
+        for i in range(len(self.jour)+1):
+            print(self.name+self.prenom+self.jour[0:i])
+
+        for i in range(len(self.mois)+1):
+            print(self.name+self.prenom+self.mois[0:i])
+
+        for i in range(len(self.anné)+1):
+            print(self.name+self.prenom+self.anné[0:i])
+   ########################################
+        for i in range(len(self.date)+1):
+            print(self.date[0:i]+self.name+self.prenom)
+
+        for i in range(len(self.jour)+1):
+            print(self.jour[0:i]+self.name+self.prenom)
+
+        for i in range(len(self.mois)+1):
+            print(self.mois[0:i]+self.name+self.prenom)
+
+        for i in range(len(self.anné)+1):
+            print(self.anné[0:i]+self.name+self.prenom)
+
+#######################################################
+
+        for i in range(len(self.date)+1):
+            print(self.date[0:i]+self.prenom+self.name)
+
+        for i in range(len(self.jour)+1):
+            print(self.jour[0:i]+self.prenom+self.name)
+
+        for i in range(len(self.mois)+1):
+            print(self.mois[0:i]+self.prenom+self.name)
+
+        for i in range(len(self.anné)+1):
+            print(self.anné[0:i]+self.prenom+self.name)
+
+########################################################
+        for i in range(len(self.date)+1):
+            print(self.prenom+self.date[0:i]+self.name)
+
+        for i in range(len(self.jour)+1):
+            print(self.prenom+self.jour[0:i]+self.name)
+
+        for i in range(len(self.mois)+1):
+            print(self.prenom+self.mois[0:i]+self.name)
+
+        for i in range(len(self.anné)+1):
+            print(self.prenom+self.anné[0:i]+self.name)
+
+######################################################
+
+        for i in range(len(self.date)+1):
+            print(self.name+self.date[0:i]+self.prenom)
+
+        for i in range(len(self.jour)+1):
+            print(self.name+self.jour[0:i]+self.prenom)
+
+        for i in range(len(self.mois)+1):
+            print(self.name+self.mois[0:i]+self.prenom)
+
+        for i in range(len(self.anné)+1):
+            print(self.name+self.anné[0:i]+self.prenom)
+
+######################################################
+
+        for i in range(len(self.date)+1):
+            print(self.prenom+self.name+self.date[0:i])
+
+        for i in range(len(self.jour)+1):
+            print(self.prenom+self.name+self.jour[0:i])
+
+        for i in range(len(self.mois)+1):
+            print(self.prenom+self.name+self.mois[0:i])
+
+        for i in range(len(self.anné)+1):
+            print(self.prenom+self.name+self.anné[0:i])
+
+########################################################
+
+        for i in range(len(self.date)+1):
+            print(self.name+self.prenom+self.date[0:i])
+
+        for i in range(len(self.jour)+1):
+            print(self.name+self.prenom+self.jour[0:i])
+
+        for i in range(len(self.mois)+1):
+            print(self.name+self.prenom+self.mois[0:i])
+
+        for i in range(len(self.anné)+1):
+            print(self.name+self.prenom+self.anné[0:i])
+
+
+print('                                                                  ')
+print('                                                                  ')
+n=str(input('            Insert Your First Attribute (ex:Name or username ... ): '))
+p=str(input('            Insert Your Second Attribute (ex:Familly Name or Girl_Friend_Name)... : '))
+j=str(input('            Insert Your Third Attribute (ex:Day Of Birth)... :'))
+m=str(input('            Insert Your fourth Attributes (ex:Month Of Birth)... : '))
+a=str(input('            Insert Your Fifth Attributes (ex:Year Of Birth)... : '))
+root=MyApp(n,p,j,m,a)
+print(root.function())
